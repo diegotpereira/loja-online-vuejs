0ab2e13fc91d1f68affd3404e0184c8b4508f0cb
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-11-09 09:51:31 -0300

    adicionado estilo no template verificar

--- src/views/Verificar.vue
+++ src/views/Verificar.vue
@@ -11,7 +11,7 @@
                 <div class="outro-cartao" v-for="item in buscarCarrinho" :key="item.produto.id">
 
                     <router-link class="cartao" :to="{ name: 'ProdutoDetalhes', params: { genero: item.produto.genero, id: item.produto.id}, }">
-                        <img src="" alt="" class="cartao-imagem">
+                        <img :src="item.produto.src" class="cartao-imagem">
                         <div class="cartao-texto">
                             <p class="item-marca">{{ item.produto.marca }}</p>
                             <p class="item-titulo">{{ item.produto.titulo }}</p>
@@ -83,7 +83,7 @@ export default {
     background-image: linear-gradient(transparent, rgb(0, 0, 0));
     text-align: center;
     justify-content: center;
-    align-items: center;
+    align-content: center;
 }
 
 .buscar-titulo {
@@ -95,6 +95,7 @@ export default {
     font-weight: 100;
     text-indent: 0.3em;
     font-size: 3rem;
+    color: #fff;
     text-transform: uppercase;
     font-family: "Magisho", sans-serif;
 }
@@ -187,6 +188,63 @@ export default {
     justify-content: center;
 }
 
+.verificar-cartao .corpo-verificar .texto-total {
+    position: relative;
+    font-family: "Magisho", sans-serif;
+    font-size: 1.2rem;
+    padding: 2em 0em;
+    margin: 0;
+    align-self: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+}
+
+.verificar-cartao .footer-verificar {
+    position: relative;
+    bottom: 0;
+    display: flex;
+    align-self: center;
+    width: 15vw;
+    height: 7vh;
+    border-top-left-radius: 2em;
+    border-top-right-radius: 2em;
+    border-radius: 2em 2em 1em 1em;
+    justify-content: center;
+    border-top: 1px solid black;
+    background-color: #eaeaea;
+}
+
+.verificar-cartao .footer-verificar .verificar {
+    margin-bottom: 0.2em;
+    display: block;
+    transition: 0.5s;
+    border: none;
+    background-color: rgb(0, 0, 0);
+    color: rgb(255, 255, 255);
+    padding: 0.8em 2em;
+    align-self: center;
+    text-align: center;
+    bottom: 0;
+    font-size: 1rem;
+    font-family: "Magisho", sans-serif;
+    width: 10vw;
+    position: absolute;
+    cursor: pointer;
+    border-radius: 0.4em;
+    margin-bottom: 0.5em;
+}
+
+.cartao-secao {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: fit-content;
+    width: 52vw;
+    padding: 0.5em 0em 5em 0em;
+    background-color: rgba(0, 0, 0);
+}
+
 
 .cartao-secao .outro-cartao {
     display: flex;
@@ -223,5 +281,112 @@ export default {
 
 .outro-cartao:hover {
     transform: translateX(-0.6%);
+}
+
+.cartao-imagem {
+    position: absolute;
+    width: inherit;
+    height: inherit;
+    object-fit: cover;
+    display: block;
+    object-position: top;
+    transition: transform 0.6s;
+    filter: brightness(80%);
+    position: relative;
+    width: 15vw;
+    height: 20vh;
+    min-width: 5vw;
+    padding: 4em 0em;
+}
+
+.outro-cartao:hover > .cartao > .cartao-imagem {
+    transform: scale(1.1);
+}
+
+.cartao-texto {
+    display: flex;
+    flex-direction: column;
+    padding: 1em 2em 1em 2em;
+    font-size: 1rem;
+    word-wrap: break-word;
+    text-align: left;
+    align-self: right;
+    color: white;
+    width: 50vw;
+}
+
+.cartao-texto > .item-titulo {
+    font-size: 1.1rem;
+    font-weight: 800;
+    color: rgb(214, 214, 214);
+    margin: 0;
+    margin-bottom: 1em;
+}
+
+.cartao-texto > .item-marca {
+    margin: 0;
+    font-size: 0.9em;
+    color: rgb(180, 180, 180);
+    font-family: "Valverde", sans-serif;
+    letter-spacing: 0.1em;
+    text-shadow: 1px 1px 2px black;
+}
+
+.cartao-texto > .item-quantidade {
+    margin: 0;
+    font-size: 0.9em;
+    color: rgb(255, 255, 255);
+    font-family: "Valverde", sans-serif;
+    letter-spacing: 0.1em;
+    text-shadow: 1px 1px 2px black;
+}
+
+.cartao-texto > .item-tamanho-texto {
+    display: flex;
+    flex-direction: row;
+    position: absolute;
+    bottom: 10px;
+    font-size: 0.9rem;
+    color: rgb(179, 179, 179);
+    font-family: "Valverde", sans-serif;
+}
+
+.cartao-texto .item-tamanho-texto .item-tamanho {
+  font-size: 0.9rem;
+  color: rgb(179, 179, 179);
+  font-family: "Valverde", sans-serif;
+  text-transform: uppercase;
+}
+
+.cartao-texto > .item-preco {
+    display: flex;
+    justify-content: right;
+    margin-top: 2em;
+    font-size: 1rem;
+    letter-spacing: 0.2em;
+    color: white;
+    font-family: "Valverde", sans-serif;
+    font-weight: 100;
+}
+
+.cancelar {
+    transform: translateY(70%) translateX(-70%);
+    position: absolute;
+    right: 0;
+    z-index: 3;
+    font-size: 1.3em;
+}
+
+.cancelar .uil {
+    display: flex;
+    position: relative;
+    cursor: pointer;
+    transition: 0.5s;
+    color: rgb(194, 194, 194);
+}
+
+
+.cancelar .uil:hover {
+    color: #fff;
 }
 </style>
